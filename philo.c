48d869922a01677f17cb324183205833fa06664c
Julio Max Calderone Da Silva <julcalde@1-C-7.42heilbronn.de>
2025-02-27 15:42:42 +0100

    ignore

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 15:02:57 by julcalde          #+#    #+#             */
-/*   Updated: 2025/02/27 15:05:45 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/02/27 15:42:17 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,17 @@
 int	main(int argc, char **argv)
 {
 	return (0);
-}
+} /* Philosophers with threads and mutexes
+	Program name:		philo
+	Turn in files:		Makefile, *.h, *.c, in directory philo/
+	Makefile:			NAME, all, clean, fclean, re
+	Arguments:			number_of_philosophers, time_to_die time_to_eat,
+						time_to_sleep,
+						[number_of_times_each_philosopher_must_eat]
+	External functs:	memset, printf, malloc, free, write, usleep,
+						gettimeofday, pthread_create, pthread_detach,
+						pthread_join, pthread_mutex_init,
+						pthread_mutex_destroy, pthread_mutex_lock,
+						pthread_mutex_unlock.
+	Libft authorized:	No
+*/
