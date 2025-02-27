93d99cd3fd2d8b78b4a0ec58dace64d2f6d3e101
Julio Max Calderone Da Silva <julcalde@1-C-7.42heilbronn.de>
2025-02-27 15:06:52 +0100

    first commit

---
+++ philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 15:02:52 by julcalde          #+#    #+#             */
+/*   Updated: 2025/02/27 15:04:58 by julcalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+#endif
