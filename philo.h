5add476e6ad1aa35d677e979f8bbca5ae7e49972
julcalde <julcalde@student.42heilbronn.de>
2025-03-04 14:49:09 +0100

    added structures in header file

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 15:02:52 by julcalde          #+#    #+#             */
-/*   Updated: 2025/03/04 14:11:14 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/03/04 14:46:47 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,45 @@
 # include <stdbool.h>
 # include <limits.h>
 
+/*
+	STRUCTURES
+*/
+typedef pthread_mutex_t	t_mutex;
+
+typedef struct s_fork
+{
+	t_mutex	fork;
+	int		fork_id;
+}			t_fork;
+
+typedef struct s_philo
+{
+	int			id;
+	long		meals_count;
+	long		last_meal;
+	bool		happy_belly;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id;
+	t_input		*input;
+}			t_philo;
+
+typedef struct s_input
+{
+	long	num_of_philos; //argv[1]
+	long	time_to_die; //argv[2]
+	long	time_to_eat; //argv[3]
+	long	time_to_sleep; //argv[4]
+	long	num_of_meals; //argv[5] ()== -1 if no limit)
+	long	time_start; //gettimeofday for timestamps
+	long	time_end; //getttimeofday for death or limit if meal reached
+	t_fork	*forks; //array of fork mutexes
+	t_philo	*philos; //array of philo threads
+}			t_input;
+
+/*
+	FUNCTIONS
+*/
 //	utils.c
 void	perror_exit(const char *err_msg);
 
