953fc70fd625d8da339b747dac7491478205c009
julcalde <julcalde@student.42heilbronn.de>
2025-03-04 13:25:41 +0100

    ignore

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 15:02:52 by julcalde          #+#    #+#             */
-/*   Updated: 2025/02/27 15:04:58 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/03/04 13:25:11 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/time.h>
+# include <stdbool.h>
+# include <limits.h>
 
 #endif
